78b105c0fc12553c6529be4aeb55de42222286bc
virtualvillagegr <132691336+virtualvillagegr@users.noreply.github.com>
2026-06-02 19:10:40 +0200

    Add Protected Blocks constants

---
+++ terrain-flattener-mod/behavior_pack/scripts/constants/ProtectedBlocks.js
@@ -0,0 +1,29 @@
+// ============================================================================
+// CONSTANTS - Protected Blocks Configuration
+// ============================================================================
+
+export const PROTECTED_BLOCKS = new Set([
+  "minecraft:diamond_ore",
+  "minecraft:deepslate_diamond_ore",
+  "minecraft:gold_ore",
+  "minecraft:iron_ore",
+  "minecraft:coal_ore",
+  "minecraft:emerald_ore",
+  "minecraft:redstone_ore",
+  "minecraft:lapis_ore",
+  "minecraft:chest",
+  "minecraft:barrel",
+  "minecraft:spawner",
+  "minecraft:shulker_box",
+  "minecraft:furnace",
+  "minecraft:blast_furnace",
+  "minecraft:smoker",
+  "minecraft:hopper",
+  "minecraft:dropper",
+  "minecraft:dispenser",
+  "minecraft:enchanting_table",
+  "minecraft:brewing_stand"
+]);
+
+export const BEDROCK_BLOCK = "minecraft:bedrock";
+export const AIR_BLOCK = "minecraft:air";
